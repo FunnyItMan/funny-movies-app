0c4bf5923d1a08d31d69bd26a4defe5f6a791cde
zhidong.li <17521151090@163.com>
2025-10-14 20:58:26 +0800

    add tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,52 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 
+import { Ionicons } from '@expo/vector-icons';
+
 const TabLayout = () => {
     return (
-        <Tabs>
-            <Tabs.Screen name="index" options={{ title: 'Home' }} />
-            <Tabs.Screen name="search" options={{ title: 'Search' }} />
-            <Tabs.Screen name="profile" options={{ title: 'Profile' }} />
+        <Tabs
+            screenOptions={{ tabBarInactiveTintColor: '#A8A8A8', tabBarActiveTintColor: '#E63946' }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Ionicons
+                            name={focused ? 'home-sharp' : 'home-outline'}
+                            color={color}
+                            size={size}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="search"
+                options={{
+                    title: 'Search',
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Ionicons
+                            name={focused ? 'search-sharp' : 'search-outline'}
+                            color={color}
+                            size={size}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: 'Profile',
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Ionicons
+                            name={focused ? 'person-sharp' : 'person-outline'}
+                            color={color}
+                            size={size}
+                        />
+                    ),
+                }}
+            />
         </Tabs>
     );
 };
